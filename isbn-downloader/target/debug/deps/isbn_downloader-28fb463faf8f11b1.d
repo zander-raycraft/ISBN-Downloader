b63916f09e4ee4a5af3804cdc56d45de19a274c3
zander-raycraft <zanderraycraft504@gmail.com>
2023-08-28 15:52:50 -0500

    started da ting

---
+++ isbn-downloader/target/debug/deps/isbn_downloader-28fb463faf8f11b1.d
@@ -0,0 +1,5 @@
+/Users/zanderraycraft/Documents/personalProjects/Rust-projects/ISBN-Downloader/isbn-downloader/target/debug/deps/isbn_downloader-28fb463faf8f11b1.rmeta: src/main.rs
+
+/Users/zanderraycraft/Documents/personalProjects/Rust-projects/ISBN-Downloader/isbn-downloader/target/debug/deps/isbn_downloader-28fb463faf8f11b1.d: src/main.rs
+
+src/main.rs:
---
+++ isbn-downloader/target/debug/deps/isbn_downloader-cbf53dc0ab8da61d.d
@@ -0,0 +1,5 @@
+/Users/zanderraycraft/Documents/personalProjects/Rust-projects/ISBN-Downloader/isbn-downloader/target/debug/deps/isbn_downloader-cbf53dc0ab8da61d.rmeta: src/main.rs
+
+/Users/zanderraycraft/Documents/personalProjects/Rust-projects/ISBN-Downloader/isbn-downloader/target/debug/deps/isbn_downloader-cbf53dc0ab8da61d.d: src/main.rs
+
+src/main.rs:
